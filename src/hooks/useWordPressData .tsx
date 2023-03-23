f68834bdeb106b1087f2d25a7112bd4c3fb5376d
dawid2806 <david-kaczmarek@gmx.de>
2023-03-23 21:10:21 +0100

    .

--- src/hooks/useWordPressData .tsx
+++ src/hooks/useWordPressData .tsx
@@ -1,16 +1,18 @@
 import { useQuery } from "react-query";
 import { Post } from "../typs/WordpressTypes";
+import process from "process";
+
 type UseWordPressDataProps = {
   endpoint: string;
 };
 
 export const useWordPressData = ({ endpoint }: UseWordPressDataProps) => {
-  const url = process.env.WORDPRESS_URL;
-
   const { data, isLoading, isError } = useQuery<Post[], Error>(
     ["wordpress", endpoint],
     async () => {
-      const response = await fetch(`${url}/${endpoint}`);
+      const response = await fetch(
+        `https://david2806.eu/wp-json/wp/v2/${endpoint}`
+      );
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
