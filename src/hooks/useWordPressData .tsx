0e1f99cf4a1e6f8ec44eab642d220bb465baadfa
dawid2806 <david-kaczmarek@gmx.de>
2023-03-26 00:38:50 +0100

    test with env

--- src/hooks/useWordPressData .tsx
+++ src/hooks/useWordPressData .tsx
@@ -8,12 +8,11 @@ type UseWordPressDataProps = {
 
 export const useWordPressData = ({ endpoint }: UseWordPressDataProps) => {
   const url = process.env.WORDPRESS_URL;
+
   const { data, isLoading, isError } = useQuery<Post[], Error>(
     ["wordpress", endpoint],
     async () => {
-      const response = await fetch(
-        `https://david2806.eu/wp-json/wp/v2/${endpoint}`
-      );
+      const response = await fetch(`${url}/${endpoint}`);
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
